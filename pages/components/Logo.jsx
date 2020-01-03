4bb1e0304e42a84892fe357b7abca43e955abe7a
Maksym Dmukhovskyy <dm.maksym@gmail.com>
2020-01-03 17:29:22 +0100

    style and three js setup

--- pages/components/Logo.jsx
+++ pages/components/Logo.jsx
@@ -1,3 +1,11 @@
-const Logo = () => <div className='pixels-logo'>LOGO</div>;
+const Logo = () => (
+  <div className='pixels-logo'>
+    <div className='pixels-logo__wrapper'>
+      <div className='pixels-logo__square pixels-logo__square--platinum' />
+      <div className='pixels-logo__square pixels-logo__square--gold' />
+      <div className='pixels-logo__square pixels-logo__square--silver' />
+    </div>
+  </div>
+);
 
 export default Logo;
--- pages/components/MainInfo.jsx
+++ pages/components/MainInfo.jsx
@@ -4,7 +4,9 @@ const MainInfo = () => (
       <span>3 PIXELS STUDIO LTD</span>
     </div>
     <div className='pixels-main-info__info'>
-      <span>Beautiful, Fast and Reliable Software</span>
+      <span className='pixels-color-gold'>Beautiful</span>,{' '}
+      <span className='pixels-color-gold'>Fast</span> and{' '}
+      <span className='pixels-color-gold'>Reliable</span> Software
     </div>
     <div className='pixels-main-info__email'>
       <a href='mailto:hello@3-pixels.com' target='_top'>
